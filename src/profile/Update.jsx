95ba9a4e335a6471fed14f1c6647a7834dbcc0a3
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-01-31 13:45:05 +0100

    Update update page

--- src/profile/Update.jsx
+++ src/profile/Update.jsx
@@ -7,18 +7,20 @@ import { accountService, alertService } from '@/_services';
 
 function Update({ history }) {
     const user = accountService.userValue;
-    const initialValues = {
-        title: user.title,
+    const initialValues = {        
         firstName: user.firstName,
         lastName: user.lastName,
         email: user.email,
+        phoneNumber: user.phoneNumber,
+        address: user.address,
+        city: user.city,
+        country: user.country,
+        state: user.state,
         password: '',
         confirmPassword: ''
     };
 
-    const validationSchema = Yup.object().shape({
-        title: Yup.string()
-            .required('Title is required'),
+    const validationSchema = Yup.object().shape({        
         firstName: Yup.string()
             .required('First Name is required'),
         lastName: Yup.string()
@@ -26,6 +28,21 @@ function Update({ history }) {
         email: Yup.string()
             .email('Email is invalid')
             .required('Email is required'),
+        phoneNumber: Yup.string()
+            .phoneNumber('Phone Number is invalid')
+            .required('Phone Number is required'),
+        address: Yup.string()
+                .address('Address is invalid')
+                .required('Address is required'),
+        city: Yup.string()
+            .city('City is invalid')
+            .required('City is required'),
+        state: Yup.string()
+            .state('State is invalid')
+            .required('State is required'),
+        country: Yup.string()
+            .country('Country is invalid')
+            .required('Country is required'),
         password: Yup.string()
             .min(6, 'Password must be at least 6 characters'),
         confirmPassword: Yup.string()
@@ -63,17 +80,7 @@ function Update({ history }) {
                 <Form>
                     <h1>Update Profile</h1>
                     <div className="form-row">
-                        <div className="form-group col">
-                            <label>Title</label>
-                            <Field name="title" as="select" className={'form-control' + (errors.title && touched.title ? ' is-invalid' : '')}>
-                                <option value=""></option>
-                                <option value="Mr">Mr</option>
-                                <option value="Mrs">Mrs</option>
-                                <option value="Miss">Miss</option>
-                                <option value="Ms">Ms</option>
-                            </Field>
-                            <ErrorMessage name="title" component="div" className="invalid-feedback" />
-                        </div>
+                        
                         <div className="form-group col-5">
                             <label>First Name</label>
                             <Field name="firstName" type="text" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '')} />
@@ -90,6 +97,31 @@ function Update({ history }) {
                         <Field name="email" type="text" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
                         <ErrorMessage name="email" component="div" className="invalid-feedback" />
                     </div>
+                    <div className="form-group col-5">
+                                <label>Phone Number</label>
+                                <Field name="phoneNumber" type="text" className={'form-control' + (errors.phoneNumber && touched.phoneNumber ? ' is-invalid' : '')} />
+                                <ErrorMessage name="phoneNumber" component="div" className="invalid-feedback" />
+                            </div>
+                            <div className="form-group col-5">
+                                <label>Address</label>
+                                <Field name="address" type="text" className={'form-control' + (errors.address && touched.address ? ' is-invalid' : '')} />
+                                <ErrorMessage name="address" component="div" className="invalid-feedback" />
+                            </div>
+                        <div className="form-group col-5">
+                                <label>City</label>
+                                <Field name="city" type="text" className={'form-control' + (errors.city && touched.city ? ' is-invalid' : '')} />
+                                <ErrorMessage name="city" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group col-5">
+                                <label>State</label>
+                                <Field name="state" type="text" className={'form-control' + (errors.state && touched.state ? ' is-invalid' : '')} />
+                                <ErrorMessage name="state" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <label>Country</label>
+                            <Field name="country" type="text" className={'form-control' + (errors.country && touched.country ? ' is-invalid' : '')} />
+                            <ErrorMessage name="country" component="div" className="invalid-feedback" />
+                        </div>
                     <h3 className="pt-3">Change Password</h3>
                     <p>Leave blank to keep the same password</p>
                     <div className="form-row">
